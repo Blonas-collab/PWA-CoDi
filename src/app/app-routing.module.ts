9223280d2824ebec273c21ea1efef929953fdf09
LowisSpross <spross@querformat.info>
2022-02-23 11:56:32 +0100

    adjusted Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CocktailsComponent } from './cocktails/cocktails.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CocktailsComponent } ,
+  { path: 'about', component: AboutComponent } ,
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
